6643bb536059579340279e234bfcd3ac2c0827ad
Joshua Holland <holland_74@hotmail.com>
2018-02-12 14:28:13 -0800

    first attempt at making a grid

---
+++ main.py
@@ -0,0 +1,26 @@
+import sys
+from tkinter import *
+import random
+
+h = 20
+w = 30
+#arr[width][height]
+arr = [[random.randint(0,1) for x in range(w)] for y in range(h)]
+
+root = Tk()                     #This creates a window, but it won't show up
+root.wm_title("Window Title") #Makes the title that will appear in the top left
+root.config(background = "#FFFFFF")
+
+def __init__():
+    for i in range(h):
+        for j in range(w):
+            test = Frame(root, width=30, height=30, bg="#FFF")
+            test.grid(row=i, column=j, padx=1, pady=1)
+            if arr[i][j]==1:
+                test.config(background = "#000")
+
+__init__()
+test = Frame(root, width=30, height=30, bg="#BBB")
+test.grid(row=5, column=5, bg="999000")
+
+root.mainloop() #start monitoring and updating the GUI
