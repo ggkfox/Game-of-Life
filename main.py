85ac6995e7c2ce2388cd78ef5caa78c94b6d7aa4
Joshua Holland <holland_74@hotmail.com>
2018-02-26 11:00:02 -0800

    complete

--- main.py
+++ main.py
@@ -1,24 +1,65 @@
+#---Game of Life---
+#By: <Joshua Holland>
+
 import sys
 from tkinter import *
+import time
 import random
 
-h = 20
-w = 30
+#-------GLOBAL VARIABLES-------
+#number of viles vertically and horizonatally
+h = 35
+w = 85
+#size of a single side of each tile.
+size = 20
 #arr[width][height]
-arr = [[random.randint(0,1) for x in range(w)] for y in range(h)]
+arr = [[1 if random.randint(0,7)==2 else 0 for x in range(w)] for y in range(h)]
+temp = [[0 for x in range(w)] for y in range(h)]
 
-root = Tk()                     #This creates a window, but it won't show up
-root.wm_title("Window Title") #Makes the title that will appear in the top left
-root.config(background = "#FFFFFF")
+#-------Functions-------
+def initializeValues():
+    print("initializing array Values....")
 
-def __init__():
+def updateValues():
+    global arr #apparently makes globals editable.
+    global temp
+    print("updating array values....")
     for i in range(h):
         for j in range(w):
-            test = Frame(root, width=30, height=30, bg="#FFF")
-            test.grid(row=i, column=j, padx=1, pady=1)
+            count = 0
+            if arr[(i-1+h)%h][(j-1+w)%w] == 1: count += 1
+            if arr[(i-1+h)%h][j]         == 1: count += 1
+            if arr[(i-1+h)%h][(j+1+w)%w] == 1: count += 1
+            if arr[i][(j-1+w)%w]         == 1: count += 1
+            if arr[i][(j+1+w)%w]         == 1: count += 1
+            if arr[(i+1+h)%h][(j-1+w)%w] == 1: count += 1
+            if arr[(i+1+h)%h][j]         == 1: count += 1
+            if arr[(i+1+h)%h][(j+1+w)%w] == 1: count += 1
+            if arr[i][j] == 0 and count == 3: temp[i][j] = 1
+            elif arr[i][j] == 1 and (count < 2 or count > 3): temp[i][j] = 0
+            else: temp[i][j] = arr[i][j]
+    arr = [[temp[i][j] for j in range(w)] for i in range(h)] #match arr to temp_array
+    updateBoard()
+
+def updateBoard():
+    canvas.delete("all")
+    for i in range(h):
+        for j in range(w):
+            xpos = j*size
+            ypos = i*size
+            rect = canvas.create_rectangle(xpos, ypos, xpos+size, ypos+size, fill='#FFF')
             if arr[i][j]==1:
-                test.config(background = "#000")
+                canvas.itemconfig(rect, fill='#99ccff')
+            else: canvas.itemconfig(rect, fill='white')
+    canvas.after(1, updateValues)
 
-__init__()
 
+#-------Build Window-------
+root = Tk()                     #This creates a window, but it won't show up
+root.wm_title("Conway's Game of Life       by: Joshua Holland") #Makes the title that will appear in the top left
+root.geometry("%dx%d+%d+%d" % (w*size, h*size+100, 130, 100))
+canvas = Canvas(root, width=w*size, height=h*size, background='black')
+canvas.pack()
+play = Button(root, text="Play", command=updateBoard) #this command starts recursive algorithm.
+play.pack()
 root.mainloop()
